47c8b9a687d458408dda569bd7ca2fdad4525999
Dipod <dipod.dopid@gmail.com>
2020-09-13 17:35:55 +0300

    Task "Greatest common factor" solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,21 @@
-/*Дана строка.
- * Найдите в этой строке второе вхождение буквы f и выведите индекс этого вхождения.
- * Если буква f в данной строке встречается только один раз, выведите число -1,
- * а если не встречается ни разу, выведите число -2.
- * Индексы нумеруются с нуля.*/
+/*В stdin даны два натуральных числа. Выведите в stdout их наибольший общий делитель.*/
 
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
+	int a, b;
+	cin >> a >> b;
 
-	string s;
-	unsigned int occurenceIterator = 0;
-	cin >> s;
-
-	for (int i = 0; i < s.size(); i++) {
-		if (s[i] == 'f') {
-			occurenceIterator++;
-		}
-		if (occurenceIterator == 2) {
-			cout << i;
-			return 0;
+	while (a != 0 and b != 0) {
+		if (a > b) {
+			a = a % b;
+		} else {
+			b = b % a;
 		}
 	}
 
-	if (occurenceIterator == 0) {
-		cout << -2;
-	} else if (occurenceIterator == 1) {
-		cout << -1;
-	}
+	cout << a + b;
 
 	return 0;
 }
