f57a613be6865d977a3c5db4bddfe7e1f2022dbe
Dipod <dipod.dopid@gmail.com>
2020-09-15 22:08:03 +0300

    Task "Queue" solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,92 @@
-/*Даны значения температуры, наблюдавшиеся в течение N подряд идущих дней.
- * Найдите номера дней (в нумерации с нуля) со значением температуры выше среднего арифметического за все N дней.
+/*Люди стоят в очереди, но никогда не уходят из её начала, зато могут приходить в конец и уходить оттуда.
+ * Более того, иногда некоторые люди могут прекращать и начинать беспокоиться из-за того,
+ * что очередь не продвигается.
+ * Реализуйте обработку следующих операций над очередью:
+ *     WORRY i: пометить i-го человека с начала очереди (в нумерации с 0) как беспокоящегося;
+ *     QUIET i: пометить i-го человека как успокоившегося;
+ *     COME k: добавить k спокойных человек в конец очереди;
+ *     COME -k: убрать k человек из конца очереди;
+ *     WORRY_COUNT: узнать количество беспокоящихся людей в очереди.
  *
- * Гарантируется, что среднее арифметическое значений температуры является целым числом.
+ * Изначально очередь пуста.
  *
  * Формат ввода
  *
- * Вводится число N, затем N неотрицательных целых чисел — значения температуры в 0-й, 1-й, ... (N−1)-й день.
+ * Количество операций Q, затем описания операций.
+ *
+ * Для каждой операции WORRY i и QUIET i гарантируется, что человек с номером i существует в очереди на момент операции.
+ *
+ * Для каждой операции COME -k гарантируется, что k не больше текущего размера очереди.
  *
  * Формат вывода
  *
- * Первое число K — количество дней, значение температуры в которых выше среднего арифметического.
- * Затем K целых чисел — номера этих дней.*/
+ * Для каждой операции WORRY_COUNT выведите одно целое число — количество беспокоящихся людей в очереди.*/
 
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-int main() {
+void worry(vector<bool> &queue) {
 
-	int N, sum = 0, input, K = 0;
+	int i = 0;
+	cin >> i;
+	queue[i] = true;
 
-	cin >> N;
-	vector<int> temperature(N), dayNumbers;
+}
 
-	for (int i = 0; i < N; i++) {
-		cin >> input;
-		sum += input;
-		temperature[i] = input;
-	}
+void quiet(vector<bool> &queue) {
 
-	for (int i = 0; i < N; i++) {
-		if (temperature[i] > sum / N) {
-			K++;
-			dayNumbers.push_back(i);
+	int i = 0;
+	cin >> i;
+	queue[i] = false;
+
+}
+
+void come(vector<bool> &queue) {
+
+	int i = 0;
+	cin >> i;
+	queue.resize(queue.size() + i);
+
+}
+
+void worry_count(const vector<bool> &queue) {
+
+	int counter = 0;
+
+	for (const auto &person : queue) {
+		if (person) {
+			counter++;
 		}
 	}
+	cout << counter << endl;
+}
 
-	cout << K << endl;
+int main() {
 
-	for (auto &day : dayNumbers) {
-		cout << day << ' ';
+	vector<bool> queue;
+	string command;
+	int Q = 0;
+
+	cin >> Q;
+
+	while (Q > 0) {
+		cin >> command;
+		if (command == "WORRY") {
+			worry(queue);
+		}
+		if (command == "QUIET") {
+			quiet(queue);
+		}
+		if (command == "COME") {
+			come(queue);
+		}
+		if (command == "WORRY_COUNT") {
+			worry_count(queue);
+		}
+		Q--;
 	}
 
 	return 0;
