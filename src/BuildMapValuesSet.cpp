8b19f6c819155c263d86d19a9a9f7ad283133ea5
Dipod <dipod.dopid@gmail.com>
2020-09-16 20:04:01 +0300

    Task "Set of map values" solution

---
+++ src/BuildMapValuesSet.cpp
@@ -0,0 +1,15 @@
+#include <map>
+#include <set>
+#include <string>
+
+using namespace std;
+
+set<string> BuildMapValuesSet(const map<int, string> &m) {
+	set<string> result;
+	for (const auto &item : m) {
+		result.insert(item.second);
+	}
+
+	return result;
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,50 @@
 /*
- Дан набор строк. Найдите количество уникальных строк в этом наборе.
- Формат ввода
-
- Сначала вводится количество строк N, затем — сами N строк, разделённые символом перевода строки.
- Все строки состоят лишь из латинских букв, цифр и символов подчёркивания.
-
- Формат вывода
-
- Выведите единственное целое число — количество уникальных строк в данном наборе.
+ Напишите функцию BuildMapValuesSet, принимающую на вход словарь map<int, string> и возвращающую множество значений этого словаря:
+ set<string> BuildMapValuesSet(const map<int, string>& m) {
+ // ...
+ }
 
  Пример
 
- Ввод
+ set<string> values = BuildMapValuesSet({
+ {1, "odd"},
+ {2, "even"},
+ {3, "odd"},
+ {4, "even"},
+ {5, "odd"}
+ });
 
- 6
- first
- second
- first
- third
- second
- second
+ for (const string& value : values) {
+ cout << value << endl;
+ }
 
  Вывод
 
- 3
+ even
+ odd
+
+ Пояснение
+
+ В этой задаче на проверку вам надо прислать файл с реализацией функции BuildMapValuesSet.
+ Этот файл не должен содержать функцию main. Если в нём будет функция main, вы получите ошибку компиляции.
  */
 
 #include <iostream>
 #include <set>
 #include <string>
+#include <map>
 
 using namespace std;
 
+set<string> BuildMapValuesSet(const map<int, string> &m);
+
 int main() {
-	int N = 0;
-	string input = "";
-	set<string> unique_strings;
-	cin >> N;
+	set<string> values = BuildMapValuesSet( { { 1, "odd" }, { 2, "even" }, { 3,
+			"odd" }, { 4, "even" }, { 5, "odd" } });
 
-	while (N > 0) {
-		cin >> input;
-		unique_strings.insert(input);
-		N--;
+	for (const string &value : values) {
+		cout << value << endl;
 	}
-
-	cout << unique_strings.size();
 
 	return 0;
 }
