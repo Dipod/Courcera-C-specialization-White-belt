9dc60fb5af712e44d697c250eb21f1d36fea3e23
Dipod <dipod.dopid@gmail.com>
2020-09-16 21:51:53 +0300

    Task "Synonyms" solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,103 @@
 /*
- Напишите функцию BuildMapValuesSet, принимающую на вход словарь map<int, string> и возвращающую множество значений этого словаря:
- set<string> BuildMapValuesSet(const map<int, string>& m) {
- // ...
- }
+ Два слова называются синонимами друг друга, если они имеют похожие значения.
+ Реализуйте следующие операции над словарём синонимов:
+
+ ADD word1 word2 — добавить в словарь пару синонимов (word1, word2).
+ COUNT word — узнать количество синонимов слова word.
+ CHECK word1 word2 — проверить, являются ли слова word1 и word2 синонимами.
+ Слова word1 и word2 считаются синонимами, если среди запросов ADD был хотя бы один запрос ADD word1 word2 или ADD word2 word1.
+
+ Замечание
+
+ Для упрощения, будем считать, что синонимы не обладают транзитивностью, то есть, если A - синоним B,
+ а B - синоним C, то из этого НЕ следует, что A - синоним C.
+
+ Формат ввода
+
+ Сначала вводится количество запросов Q, затем Q строк с описаниями запросов.
+ Гарантируется, что в каждом запросе CHECK и ADD слова word1 и word2 различны.
+ Все слова состоят лишь из латинских букв, цифр и символов подчёркивания.
+
+ Формат вывода
+
+ Для каждого запроса в соответствующей строке выведите ответ на него:
+
+ В ответ на запрос COUNT word выведите единственное целое число — количество синонимов слова word.
+ В ответ на запрос CHECK word1 word2 выведите строку YES, если word1 и word2 являются синонимами, и NO в противном случае.
 
  Пример
 
- set<string> values = BuildMapValuesSet({
- {1, "odd"},
- {2, "even"},
- {3, "odd"},
- {4, "even"},
- {5, "odd"}
- });
+ Ввод
 
- for (const string& value : values) {
- cout << value << endl;
- }
+ 8
+ ADD program code
+ COUNT cipher
+ ADD code cipher
+ COUNT code
+ COUNT program
+ CHECK code program
+ CHECK program cipher
+ CHECK cpp java
 
  Вывод
 
- even
- odd
-
- Пояснение
-
- В этой задаче на проверку вам надо прислать файл с реализацией функции BuildMapValuesSet.
- Этот файл не должен содержать функцию main. Если в нём будет функция main, вы получите ошибку компиляции.
+ 0
+ 2
+ 1
+ YES
+ NO
+ NO
  */
 
 #include <iostream>
-#include <set>
-#include <string>
 #include <map>
+#include <string>
+#include <set>
 
 using namespace std;
 
-set<string> BuildMapValuesSet(const map<int, string> &m);
+class Synonyms {
+private:
+	map<string, set<string>> m_synonyms;
+public:
+
+	void add(const string &word1, const string &word2) {
+		m_synonyms[word1].insert(word2);
+		m_synonyms[word2].insert(word1);
+	}
+
+	void count(const string &word) {
+		cout << m_synonyms[word].size() << endl;
+	}
+
+	void check(const string &word1, const string &word2) {
+		cout << (m_synonyms[word1].count(word2) > 0 ? "YES" : "NO") << endl;
+	}
+};
 
 int main() {
-	set<string> values = BuildMapValuesSet( { { 1, "odd" }, { 2, "even" }, { 3,
-			"odd" }, { 4, "even" }, { 5, "odd" } });
 
-	for (const string &value : values) {
-		cout << value << endl;
+	string command = "", word1 = "", word2 = "";
+	int Q = 0;
+	Synonyms synonyms;
+
+	cin >> Q;
+
+	while (Q > 0) {
+		cin >> command;
+		if (command == "ADD") {
+			cin >> word1 >> word2;
+			synonyms.add(word1, word2);
+		}
+		if (command == "COUNT") {
+			cin >> word1;
+			synonyms.count(word1);
+		}
+		if (command == "CHECK") {
+			cin >> word1 >> word2;
+			synonyms.check(word1, word2);
+		}
+		Q--;
 	}
 
 	return 0;
