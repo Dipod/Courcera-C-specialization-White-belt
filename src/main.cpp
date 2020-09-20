965ec9aa54b51ffbf295ab35b997ec5e8e8557c5
Dipod <dipod.dopid@gmail.com>
2020-09-20 19:08:30 +0300

    Task "Reversible function" solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,86 +1,114 @@
 /*
- Дана структура LectureTitle:
+ Реализуйте рассказанный на лекции класс Function, позволяющий создавать, вычислять и инвертировать функцию, состоящую из следующих элементарных операций:
 
- struct LectureTitle {
- string specialization;
- string course;
- string week;
+ прибавить вещественное число x;
+ вычесть вещественное число x.
+
+ При этом необходимо объявить константными все методы, которые по сути такими являются.
+
+ Замечание
+
+ Более детальное описание задачи с подробным разбором реализации вышеуказанного класса приводится в двух предшествующих видеолекциях.
+ На проверку пришлите файл, содержащий реализацию вышеуказанного класса Function.
+
+ Пример
+
+ Код
+
+ struct Image {
+ double quality;
+ double freshness;
+ double rating;
  };
 
- Допишите конструктор и структуры Specialization, Course, Week так, чтобы объект LectureTitle можно было создать с помощью кода
+ struct Params {
+ double a;
+ double b;
+ double c;
+ };
 
- LectureTitle title(
- Specialization("C++"),
- Course("White belt"),
- Week("4th")
- );
+ Function MakeWeightFunction(const Params& params,
+ const Image& image) {
+ Function function;
+ function.AddPart('-', image.freshness * params.a + params.b);
+ function.AddPart('+', image.rating * params.c);
+ return function;
+ }
 
- но нельзя было с помощью следующих фрагментов кода:
+ double ComputeImageWeight(const Params& params, const Image& image) {
+ Function function = MakeWeightFunction(params, image);
+ return function.Apply(image.quality);
+ }
 
- LectureTitle title("C++", "White belt", "4th");
+ double ComputeQualityByWeight(const Params& params,
+ const Image& image,
+ double weight) {
+ Function function = MakeWeightFunction(params, image);
+ function.Invert();
+ return function.Apply(weight);
+ }
 
- LectureTitle title(string("C++"), string("White belt"), string("4th"));
+ int main() {
+ Image image = {10, 2, 6};
+ Params params = {4, 2, 6};
+ cout << ComputeImageWeight(params, image) << endl;
+ cout << ComputeQualityByWeight(params, image, 46) << endl;
+ return 0;
+ }
 
- LectureTitle title = {"C++", "White belt", "4th"};
+ Вывод
 
- LectureTitle title = {{"C++"}, {"White belt"}, {"4th"}};
-
- LectureTitle title(
- Course("White belt"),
- Specialization("C++"),
- Week("4th")
- );
-
- LectureTitle title(
- Specialization("C++"),
- Week("4th"),
- Course("White belt")
- );
+ 36
+ 20
  */
 
-#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-struct Specialization {
-	string value;
-	explicit Specialization(const string &new_value) {
+class FunctionPart {
+public:
+	FunctionPart(const char &new_operation, const double &new_value) {
+		operation = new_operation;
 		value = new_value;
 	}
-};
-
-struct Course {
-	string value;
-	explicit Course(const string &new_value) {
-		value = new_value;
+	double Apply(const double &source_value) const {
+		if (operation == '+') {
+			return source_value + value;
+		} else {
+			return source_value - value;
+		}
 	}
-};
-
-struct Week {
-	string value;
-	explicit Week(const string &new_value) {
-		value = new_value;
+	void Invert() {
+		if (operation == '+') {
+			operation = '-';
+		} else if (operation == '-') {
+			operation = '+';
+		}
 	}
+private:
+	char operation;
+	double value;
 };
-
-struct LectureTitle {
-	string specialization;
-	string course;
-	string week;
-
-	LectureTitle(const Specialization &new_specialization,
-			const Course &new_course, const Week &new_week) {
-		specialization = new_specialization.value;
-		course = new_course.value;
-		week = new_week.value;
+class Function {
+public:
+	void AddPart(const char &operation, const double &value) {
+		parts.push_back( { operation, value });
 	}
+	double Apply(double value) const {
+		for (const FunctionPart &part : parts) {
+			value = part.Apply(value);
+		}
+		return value;
+	}
+	void Invert() {
+		for (FunctionPart &part : parts) {
+			part.Invert();
+		}
+		reverse(begin(parts), end(parts));
+	}
+private:
+	vector<FunctionPart> parts;
 };
-
-int main() {
-
-	LectureTitle title(Specialization("C++"), Course("White belt"),
-			Week("4th"));
-
-	return 0;
-
-}
