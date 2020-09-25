4c8f6dba22506777b0e8ca5c612e53b2a12056aa
Dipod <dipod.dopid@gmail.com>
2020-09-25 09:03:21 +0300

    Task "Fraction calculator" solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,185 @@
 /*
- Напишите функцию
+ Итак, теперь у нас с вами есть полноценный тип для представления обыкновенных дробей:
+ класс Rational интегрирован в язык с помощью перегрузки операторов и не позволяет выполнять
+ некорректные операции за счёт выбрасывания исключений.
+ Давайте на основе класса Rational создадим простой калькулятор для обыкновенных дробей.
 
- void EnsureEqual(const string& left, const string& right);
+ Вам надо написать программу, которая считывает из стандартного ввода одну строку в формате
+ ДРОБЬ_1 операция ДРОБЬ_2. ДРОБЬ_1 и ДРОБЬ_2 имеют формат X/Y, где X — целое, а Y — целое неотрицательное число.
+ операция — это один из символов '+', '-', '*', '/'.
 
- В случае, если строка left не равна строке right,
- функция должна выбрасывать исключение runtime_error с содержанием "<l> != <r>", где <l> и <r> - строки,
- которые хранятся в переменных left и right соответственно.
- Обратите внимание, что вокруг знака неравенства в строке,
- которая помещается в исключение, должно быть ровно по одному пробелу.
+ Если ДРОБЬ_1 или ДРОБЬ_2 является некорректной обыкновенной дробью,
+ ваша программа должна вывести в стандартный вывод сообщение "Invalid argument".
+ Если считанная операция — это деление на ноль, выведите в стандартный вывод сообщение "Division by zero".
+ В противном случае выведите результат операции.
 
- Если left == right, функция должна корректно завершаться.
-
- Например, код
-
- int main() {
- try {
- EnsureEqual("C++ White", "C++ White");
- EnsureEqual("C++ White", "C++ Yellow");
- } catch (runtime_error& e) {
- cout << e.what() << endl;
- }
- return 0;
- }
-
- должен выводить
-
- C++ White != C++ Yellow
+ Пример
+ stdin		stdout
+ 1/2 + 1/3	5/6
+ 1/2 + 5/0	Invalid argument
+ 4/5 / 0/8	Division by zero
  */
 
 #include <iostream>
+#include <algorithm>
 #include <sstream>
-#include <string>
 #include <exception>
 
 using namespace std;
 
-void EnsureEqual(const string &left, const string &right) {
-	if (left != right) {
-		string error = left + " != " + right;
-		throw runtime_error(error);
+int lcm(const int &first, const int &second) {
+	return (first / __gcd(first, second)) * second;
+}
+
+class Rational {
+public:
+	Rational() {
+		m_numerator = 0;
+		m_denominator = 1;
+	}
+
+	Rational(const int &numerator, const int &denominator) {
+		if (denominator == 0) {
+			throw invalid_argument("Invalid argument");
+		}
+		if (numerator == 0) {
+			m_numerator = 0;
+			m_denominator = 1;
+			return;
+		}
+
+		int divisor = __gcd(numerator, denominator);
+		m_numerator = numerator / divisor;
+		m_denominator = denominator / divisor;
+
+		if (m_denominator < 0) {
+			m_numerator = -m_numerator;
+			m_denominator = -m_denominator;
+		}
+	}
+
+	int Numerator() const {
+		return m_numerator;
+	}
+
+	int Denominator() const {
+		return m_denominator;
+	}
+
+private:
+	int m_numerator;
+	int m_denominator;
+};
+
+bool operator==(const Rational &lhs, const Rational &rhs) {
+	return (lhs.Numerator() == rhs.Numerator()
+			&& lhs.Denominator() == rhs.Denominator());
+}
+
+Rational operator+(const Rational &lhs, const Rational &rhs) {
+	if (lhs.Denominator() == rhs.Denominator()) {
+		return Rational(lhs.Numerator() + rhs.Numerator(), lhs.Denominator());
+	} else {
+		int new_denominator = lcm(lhs.Denominator(), rhs.Denominator());
+		int new_numerator = (new_denominator / lhs.Denominator())
+				* lhs.Numerator()
+				+ (new_denominator / rhs.Denominator()) * rhs.Numerator();
+		return Rational(new_numerator, new_denominator);
+	}
+}
+
+Rational operator-(const Rational &lhs, const Rational &rhs) {
+	if (lhs.Denominator() == rhs.Denominator()) {
+		return Rational(lhs.Numerator() - rhs.Numerator(), lhs.Denominator());
+	} else {
+		int new_denominator = lcm(lhs.Denominator(), rhs.Denominator());
+		int new_numerator = (new_denominator / lhs.Denominator())
+				* lhs.Numerator()
+				- (new_denominator / rhs.Denominator()) * rhs.Numerator();
+		return Rational(new_numerator, new_denominator);
+	}
+}
+
+Rational operator*(const Rational &lhs, const Rational &rhs) {
+	return Rational(lhs.Numerator() * rhs.Numerator(),
+			lhs.Denominator() * rhs.Denominator());
+}
+
+Rational operator/(const Rational &lhs, const Rational &rhs) {
+	if (rhs.Numerator() == 0) {
+		throw domain_error("Division by zero");
+	}
+
+	return Rational(lhs.Numerator() * rhs.Denominator(),
+			lhs.Denominator() * rhs.Numerator());
+}
+
+istream& operator>>(istream &stream, Rational &fraction) {
+	int numerator = fraction.Numerator(), denominator = fraction.Denominator();
+	char separator = ' ';
+
+	stream >> numerator >> separator >> denominator;
+
+	if (!stream.fail() && separator == '/') {
+		fraction = Rational(numerator, denominator);
+	}
+
+	return stream;
+}
+
+ostream& operator<<(ostream &stream, const Rational &fraction) {
+	stream << fraction.Numerator() << '/' << fraction.Denominator();
+	return stream;
+}
+
+bool operator<(const Rational &lhs, const Rational &rhs) {
+	if (lhs.Denominator() == rhs.Denominator()) {
+		return lhs.Numerator() < rhs.Numerator();
+	} else {
+		int new_denominator = lcm(lhs.Denominator(), rhs.Denominator());
+		return (new_denominator / lhs.Denominator()) * lhs.Numerator()
+				< (new_denominator / rhs.Denominator()) * rhs.Numerator();
 	}
 }
 
 int main() {
+
+	Rational lhs, rhs;
+	char operation;
+
 	try {
-		EnsureEqual("C++ White", "C++ White");
-		EnsureEqual("C++ White", "C++ Yellow");
-	} catch (runtime_error &e) {
-		cout << e.what() << endl;
+		cin >> lhs >> operation >> rhs;
+	} catch (invalid_argument &ex) {
+		cout << ex.what();
+		return 0;
 	}
+
+	if (cin.fail()) {
+		cout << "Invalid argument";
+		return 0;
+	}
+
+	switch (operation) {
+	case '/':
+		try {
+			cout << lhs / rhs;
+		} catch (domain_error &ex) {
+			cout << ex.what();
+		}
+		break;
+	case '*':
+		cout << lhs * rhs;
+		break;
+	case '-':
+		cout << lhs - rhs;
+		break;
+	case '+':
+		cout << lhs + rhs;
+		break;
+	default:
+		cout << "Invalid argument";
+		break;
+	}
+
 	return 0;
 }
