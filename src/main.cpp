da57fa23855c383552a4899c71f58f939c3b0c89
Dipod <dipod.dopid@gmail.com>
2020-10-06 08:18:32 +0300

    Task "Exceptions in class Rational" solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,68 @@
+/*
+ Вспомним класс Rational, который мы реализовали в задачах, посвящённых перегрузке операторов.
+ Там специально говорилось, что в тестах объекты класса Rational никогда не создаются с нулевым знаменателем и никогда не выполняется деление на ноль.
+ Настало время избавиться от этого ограничения и научиться обрабатывать нулевой знаменатель и деление на ноль. В этой задаче вам предстоит это сделать.
+
+ Поменяйте реализацию конструктора класса Rational так, чтобы он выбрасывал исключение invalid_argument, если знаменатель равен нулю.
+ Кроме того, переделайте реализацию оператора деления для класса Rational так, чтобы он выбрасывал исключение domain_error, если делитель равен нулю.
+
+ Напомним, что вам надо реализовать интерфейс
+
+ class Rational {
+ public:
+ Rational();
+ Rational(int numerator, int denominator);
+
+ int Numerator() const;
+ int Denominator() const;
+ };
+
+ и оператор деления для класса Rational
+
+ Rational operator / (const Rational& lhs, const Rational& rhs);
+
+ Так же, как и в задачах на перегрузку операторов, вам будет предложен файл с заготовкой кода.
+ Функция main в этом файле содержит ряд юнит-тестов, которые позволят вам проверить свою реализацию.
+ При проверке вашего решения оно будет тестироваться на более полном наборе тестов,
+ так что успешное прохождение всех юнит-тестов не гарантирует, что ваша реализация будет зачтена.
+
+ Шаблон программы:
+
+ #include <iostream>
+ #include <exception>
+ using namespace std;
+
+ class Rational {
+ // Вставьте сюда реализацию класса Rational
+ };
+
+ // Вставьте сюда реализацию operator / для класса Rational
+
+ int main() {
+ try {
+ Rational r(1, 0);
+ cout << "Doesn't throw in case of zero denominator" << endl;
+ return 1;
+ } catch (invalid_argument&) {
+ }
+
+ try {
+ auto x = Rational(1, 2) / Rational(0, 1);
+ cout << "Doesn't throw in case of division by zero" << endl;
+ return 2;
+ } catch (domain_error&) {
+ }
+
+ cout << "OK" << endl;
+ return 0;
+ }
+
+ */
+
 #include <iostream>
 #include <algorithm>
 #include <sstream>
-#include <set>
-#include <map>
-#include <vector>
+#include <exception>
 
 using namespace std;
 
@@ -19,7 +78,9 @@ public:
 	}
 
 	Rational(const int &numerator, const int &denominator) {
-
+		if (denominator == 0) {
+			throw invalid_argument("Invalid argument");
+		}
 		if (numerator == 0) {
 			m_numerator = 0;
 			m_denominator = 1;
@@ -82,7 +143,12 @@ Rational operator*(const Rational &lhs, const Rational &rhs) {
 	return Rational(lhs.Numerator() * rhs.Numerator(),
 			lhs.Denominator() * rhs.Denominator());
 }
+
 Rational operator/(const Rational &lhs, const Rational &rhs) {
+	if (rhs.Numerator() == 0) {
+		throw domain_error("Division by zero");
+	}
+
 	return Rational(lhs.Numerator() * rhs.Denominator(),
 			lhs.Denominator() * rhs.Numerator());
 }
@@ -116,35 +182,18 @@ bool operator<(const Rational &lhs, const Rational &rhs) {
 }
 
 int main() {
-	{
-		const set<Rational> rs = { { 1, 2 }, { 1, 25 }, { 3, 4 }, { 3, 4 }, { 1,
-				2 } };
-		if (rs.size() != 3) {
-			cout << "Wrong amount of items in the set" << endl;
-			return 1;
-		}
-
-		vector<Rational> v;
-		for (auto x : rs) {
-			v.push_back(x);
-		}
-		if (v != vector<Rational> { { 1, 25 }, { 1, 2 }, { 3, 4 } }) {
-			cout << "Rationals comparison works incorrectly" << endl;
-			return 2;
-		}
+	try {
+		Rational r(1, 0);
+		cout << "Doesn't throw in case of zero denominator" << endl;
+		return 1;
+	} catch (invalid_argument&) {
 	}
 
-	{
-		map<Rational, int> count;
-		++count[ { 1, 2 }];
-		++count[ { 1, 2 }];
-
-		++count[ { 2, 3 }];
-
-		if (count.size() != 2) {
-			cout << "Wrong amount of items in the map" << endl;
-			return 3;
-		}
+	try {
+		auto x = Rational(1, 2) / Rational(0, 1);
+		cout << "Doesn't throw in case of division by zero" << endl;
+		return 2;
+	} catch (domain_error&) {
 	}
 
 	cout << "OK" << endl;
